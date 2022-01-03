c5127d883aeb22bc22c777dd7fd775cbe1cdf4eb
Olivier Crété <olivier.crete@epitech.eu>
2022-01-03 15:19:34 +0100

    Balance component

---
+++ front/src/components/Balance.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { Badge } from 'spaper'
+
+  export let amount: number
+</script>
+
+<Badge type="secondary">
+  {amount}
+  <img src="eth.png" alt="ethers" />
+</Badge>
+
+<style lang="scss">
+img {
+  all: initial;
+  width: 20px;
+  vertical-align: bottom;
+}
+</style>
